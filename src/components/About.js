b73f8cb553d9f19fa67829881f92a60e1fe2afc2
Nirmal Gautam <nirmalg626@gmail.com>
2023-11-28 13:12:02 -0600

    Made About us page responsive

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
-        <div className="flex flex-col h-full w-auto mx-40 justify-center  tracking-wider">
-                <span className="font-bold text-5xl">About Me</span>
-                <hr className= "mt-8 mb-2"></hr>
-                <p className="text-xl leading-9">
+        <div className="flex flex-col h-full mx-auto justify-center tracking-wider p-4">
+                <span className="font-bold text-3xl sm:text-5xl">About Me</span>
+               <hr className= "mt-4 sm:mt-8 mb-2"></hr>
+                <p className="text-lg sm:text-xl leading-7 sm:leading-9">
                     <span className="text-gray-200 italic">Welcome!</span><br />
                     My name is <span className="text-gray-200 font-bold">Nirmal Gautam</span>.<br />
                     I am a <span className="text-gray-200 font-bold">Computer Science Junior</span>.<br />
@@ -16,9 +16,9 @@ const About = () => {
                                                     </Link>
                                             </span>.<br />         
                 </p>
-                <hr className= "mt-3 mb-4"></hr>
+                <hr className= "mt-3 mb-2 sm:mb-4"></hr>
                 <p className="text-right font-serif italic text-gray-200">Turn coffee into code</p>
-                <hr className= "mt-4"></hr>
+                <hr className= "mt-2 sm:mt-4"></hr>
 
         </div>
     );
