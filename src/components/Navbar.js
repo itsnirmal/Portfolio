d06340e2e06566e3eb077ee59d09b3a683c5898c
Nirmal Gautam <66936373+itsnirmal@users.noreply.github.com>
2024-02-09 18:47:19 -0600

    Small tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,9 +15,9 @@ const Navbar = () => {
   return (
     <div className="bg-zinc-900 w-screen h-14 top-0 z-3 absolute flex flex-wrap md:flex-nowrap items-center">
       <Link className="block px-14 py-auto md:py-0 order-1" to="/">
-        <span className="transition-all ease-in-out delay-150 block mx-14 my-auto font-bold text-2xl hover:scale-110">itsnirmal</span>
+        <span className="transition-all ease-in-out delay-150 block mx-8 my-auto font-bold text-2xl hover:scale-110">itsnirmal</span>
       </Link>
-      <button onClick={toggleMenu} className="transition-all ease-in-out delay-150 hover:scale-125 md:hidden text-white p-2 absolute right-16">
+      <button onClick={toggleMenu} className="transition-all ease-in-out delay-150 hover:scale-125 md:hidden text-white p-2 absolute right-8">
         {showMenu ? <FiX /> : <FiMenu />}
       </button>
       <nav className={`md:flex md:space-x-12 absolute right-0 pr-24 mr-12 md:mr-0 ${showMenu ? 'flex flex-col absolute top-14' : 'hidden'}`}>
